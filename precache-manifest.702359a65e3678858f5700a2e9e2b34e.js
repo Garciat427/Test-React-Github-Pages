b1a5edadfefa0e25cfb041e0783c006cb72722f6
Troy Garcia <tg1212@outlook.com>
2019-10-31 12:27:37 -0400

    Updates

---
+++ precache-manifest.702359a65e3678858f5700a2e9e2b34e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86965138152cf005c90316346b662f6",
+    "url": "/Test-React-Github-Pages/index.html"
+  },
+  {
+    "revision": "7c5463ae2f75fc07719f",
+    "url": "/Test-React-Github-Pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d04ec95501b199b14c41",
+    "url": "/Test-React-Github-Pages/static/js/2.0f1015b7.chunk.js"
+  },
+  {
+    "revision": "7c5463ae2f75fc07719f",
+    "url": "/Test-React-Github-Pages/static/js/main.a1cedaee.chunk.js"
+  },
+  {
+    "revision": "07a06077b47a433b393a",
+    "url": "/Test-React-Github-Pages/static/js/runtime-main.4b1e9419.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Test-React-Github-Pages/static/media/logo.25bf045c.svg"
+  }
+]);
